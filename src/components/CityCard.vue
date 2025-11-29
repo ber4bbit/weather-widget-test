e7a9a2b861920b40faf8531e90a8651290cd952d
Egor Mikhaylov <ber4bbit@gmail.com>
2025-11-30 01:44:14 +0700

    Add: Weather icon to city card component

--- src/components/CityCard.vue
+++ src/components/CityCard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onBeforeMount, ref, computed } from "vue";
 import { getCurrentWeatherData } from "@/api";
+import WeatherIcon from "@/components/WeatherIcon.vue";
 
 interface ICity {
     name: string;
@@ -16,12 +17,9 @@ interface IProps {
 const props = defineProps<IProps>();
 const expandedCity = ref();
 const isLoading = ref<boolean>(false);
-
-// const weatherIcon = computed(() => {
-//     if (expandedCity.value.weather.main === 'Clear') {
-//        return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><defs><linearGradient id="a" x1="26.75" x2="37.25" y1="22.91" y2="41.09" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fbbf24"/><stop offset=".45" stop-color="#fbbf24"/><stop offset="1" stop-color="#f59e0b"/></linearGradient></defs><circle cx="32" cy="32" r="10.5" fill="url(#a)" stroke="#f8af18" stroke-miterlimit="10" stroke-width=".5"/><path fill="none" stroke="#fbbf24" stroke-linecap="round" stroke-miterlimit="10" stroke-width="3" d="M32 15.71V9.5m0 45v-6.21m11.52-27.81l4.39-4.39M16.09 47.91l4.39-4.39m0-23l-4.39-4.39m31.82 31.78l-4.39-4.39M15.71 32H9.5m45 0h-6.21"><animateTransform attributeName="transform" dur="45s" repeatCount="indefinite" type="rotate" values="0 32 32; 360 32 32"/></path></svg>
-//     }
-// })
+const weatherState = computed(() => (
+	expandedCity.value.weather[0].main
+))
 
 onBeforeMount(() => {
     isLoading.value = true;
@@ -37,7 +35,7 @@ onBeforeMount(() => {
     <div v-else class="city-card">
         <span class="city-card__title">{{`${expandedCity.name}, ${expandedCity.sys.country}`}}</span>
         <div class="city-card__temperature">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><defs><linearGradient id="a" x1="26.75" x2="37.25" y1="22.91" y2="41.09" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fbbf24"/><stop offset=".45" stop-color="#fbbf24"/><stop offset="1" stop-color="#f59e0b"/></linearGradient></defs><circle cx="32" cy="32" r="10.5" fill="url(#a)" stroke="#f8af18" stroke-miterlimit="10" stroke-width=".5"/><path fill="none" stroke="#fbbf24" stroke-linecap="round" stroke-miterlimit="10" stroke-width="3" d="M32 15.71V9.5m0 45v-6.21m11.52-27.81l4.39-4.39M16.09 47.91l4.39-4.39m0-23l-4.39-4.39m31.82 31.78l-4.39-4.39M15.71 32H9.5m45 0h-6.21"><animateTransform attributeName="transform" dur="45s" repeatCount="indefinite" type="rotate" values="0 32 32; 360 32 32"/></path></svg>
+			<WeatherIcon :weather="weatherState" />
             <span>{{Math.round(expandedCity.main.temp)}}°C</span>
         </div>
     </div>
